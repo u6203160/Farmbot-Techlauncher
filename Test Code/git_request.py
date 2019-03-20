c2618c1bbd7c2bc3bebf9c1a5fb8773800376d19
zizheng huang <huangzizheng98@gmail.com>
2019-03-20 18:16:59 +1100

    use python request to download files from git

---
+++ Test Code/git_request.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 20 16:50:52 2019
+
+@author: huang
+"""
+
+import requests
+from os import getcwd
+
+
+filename = getcwd() + 'somefile.py'
+#put the file under the current working directory
+url = 'https://raw.githubusercontent.com/zizhenghuang/Farmbot-Techlauncher/master/Test%20Code/gitget.py'
+#use the "raw" or the full github view of the file will be downloaded, not just the contents
+r=requests.get(url)
+
+with open(filename,'wb') as f:
+    f.write(r.content)
+
+
