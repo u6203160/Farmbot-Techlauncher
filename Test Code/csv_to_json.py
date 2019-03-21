0253f49c7937eb8f85cb7a3ecd2434d6d67766b3
Wei  Ren <u6233682@anu.edu.au>
2019-03-21 14:12:33 +1100

    test1

--- Test Code/csv_to_json.py
+++ Test Code/csv_to_json.py
@@ -1,1 +1,13 @@
+import csv
+import json
 
+#Read the input.csv and convert it to output.json
+csvfile = open('input.csv', 'r')
+jsonfile = open('output.json', 'w')
+
+#Sort each row with cooresponding attribute to json format
+fieldnames = ("")
+reader = csv.DictReader(csvfile, fieldnames)
+for row in reader:
+    json.dump(row, jsonfile)
+    jsonfile.write('\n')
