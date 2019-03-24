c004cb133138f515d2422ccc2097555d8d3da9c1
Patrick <patrick@iisys.com.au>
2019-03-24 15:00:20 +1100

    Fixed api gen by adding username and password fields

--- farmbot/api_token_gen.py
+++ farmbot/api_token_gen.py
@@ -3,33 +3,20 @@ import os
 import json
 import urllib.request
 
-
 load_dotenv()
 
+data = {'user': {'email': os.getenv("EMAIL"), 'password': os.getenv("PASSWORD")}}
 
-# Get an API token using email / password.
-data = {'user': {'email': 'user@user.com', 'password': 'password123'}}
-
-# To generate the token, we POST JSON to `api/tokens`.
 req = urllib.request.Request('https://my.farmbot.io/api/tokens')
 req.add_header('Content-Type', 'application/json')
 response = urllib.request.urlopen(req, json.dumps(data).encode('utf-8'))
 
-# It will return a JSON object...
 raw_json = response.read()
 token_data = json.loads(raw_json)
 
-# ... response_body.token.encoded is the important part- you will use this as
-# an MQTT password.
 the_token = token_data['token']['encoded']
 
 print("\n\nYou will need to know your device_id to use MQTT.\n")
 print("Your device id is: \n" + token_data['token']['unencoded']['bot'] + "\n")
 print("The MQTT Host is: \n" + token_data['token']['unencoded']['mqtt'] + "\n")
-# You can copy/paste this data and move on to `subscribe_example.py`.
 print("The token is: \n" + the_token)
-
-
-
-print(os.getenv("EMAIL"))
-print(os.getenv("PASSWORD"))
